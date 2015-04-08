b8e5d67e66a464de61cfa550a6935ea1cdcadc88
MyExamples <kunal.deshmukh@hotmail.com>
2015-04-08 13:12:56 +0530

    ScreenShot For Nidhi and Ninad

---
+++ ScreenShotHtml2Canvas/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScreenShotHtml2Canvas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ScreenShot(FormCollection formCollection)
+        {
+            string screenShot = formCollection["capturedShot"];
+            //remove the image header details
+            string trimmedData = screenShot.Replace("data:image/png;base64,", "");
+
+            //convert the base 64 string image to byte array
+            byte[] uploadedImage = Convert.FromBase64String(trimmedData);
+
+            //the byte array can be saved into database or on file system
+            //saving the image on the server
+            string fileName = Guid.NewGuid() + ".png";
+            string path = Server.MapPath("~/App_Data/" + fileName);
+            System.IO.File.WriteAllBytes(path, uploadedImage);
+            return View("Index");
+        }
+       
+    }
+}
---
+++ ScreenShotHtml2Canvas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScreenShotHtml2Canvas.MvcApplication" Language="C#" %>
